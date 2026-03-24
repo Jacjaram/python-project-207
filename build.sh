2922d7c6ce05c4a139a64562a76a3cc23201856f
Jacobo Jaramillo <jacobo.jaramillo@hotmail.com>
2026-03-23 20:10:06 -0700

    Recontruir build

--- build.sh
+++ build.sh
@@ -3,8 +3,11 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
-# Instala dependencias del proyecto usando Poetry
+# Instala dependencias del proyecto con Poetry
 poetry install --no-root
+
+# Instala Gunicorn en el entorno uv
+uv run pip install gunicorn
 
 # Sincroniza uv
 uv sync
